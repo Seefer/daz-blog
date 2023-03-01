258bf48c31df12584cc9ce20154b462cfe486b09
Darren Evans <seefer@outlook.com>
2023-03-01 11:37:03 +0000

    build: adding lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,21 @@
+// next lint command builder as per lint-staged instructions:
+// https://nextjs.org/docs/basic-features/eslint
+
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
 module.exports = {
+  // run next lint
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+
   // Check Typescript files
   '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
 
-  // Lint and format TypeScript and                                             //JavaScript files
+  // Lint and format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
     `pnpm eslint --fix ${filenames.join(' ')}`,
     `pnpm prettier --write ${filenames.join(' ')}`,
