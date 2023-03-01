c2d2e94aa3786fe05758c9a0dc666cf079d7d1a6
Darren Evans <seefer@outlook.com>
2023-03-01 12:33:06 +0000

    build: adding lint-staged8

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Check Typescript files
-  './src/**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
 
   // Lint and format TypeScript and JavaScript files
-  './src/**/*.(ts|tsx|js)': (filenames) => [
+  '**/*.(ts|tsx|js)': (filenames) => [
     `pnpm eslint --fix ${filenames.join(' ')}`,
     `pnpm prettier --loglevel error --write ${filenames.join(' ')}`,
   ],
 
   // Format HTML, CSS, MarkDown and JSON
-  './src/**/*.(html|css|md|json)': (filenames) =>
+  '**/*.(html|css|md|json)': (filenames) =>
     `pnpm prettier --write ${filenames.join(' ')}`,
 };
