9ac415c3265d882e36e39d2a8024bc76e2e49636
Darren Evans <seefer@outlook.com>
2023-03-01 12:44:03 +0000

    build: adding lint-staged8

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,25 @@
+// next lint command builder as per lint-staged instructions:
+// https://nextjs.org/docs/basic-features/eslint
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
 module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+
   // Check Typescript files
-  './src/**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
 
   // Lint and format TypeScript and JavaScript files
-  './src/**/*.(ts|tsx|js)': (filenames) => [
+  '**/*.(ts|tsx|js)': (filenames) => [
     `pnpm eslint --fix ${filenames.join(' ')}`,
-    `pnpm prettier --loglevel error --write ${filenames.join(' ')}`,
+    `pnpm prettier --write ${filenames.join(' ')}`,
   ],
 
   // Format HTML, CSS, MarkDown and JSON
-  './src/**/*.(html|css|md|json)': (filenames) =>
+  '**/*.(html|css|md|json)': (filenames) =>
     `pnpm prettier --write ${filenames.join(' ')}`,
 };
