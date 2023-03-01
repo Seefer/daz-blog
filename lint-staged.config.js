b2d97293a60de22b739c177dcffe6637f8b01d42
Darren Evans <seefer@outlook.com>
2023-03-01 12:17:19 +0000

    build: adding lint-staged8

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,27 +1,27 @@
 // next lint command builder as per lint-staged instructions:
 // https://nextjs.org/docs/basic-features/eslint
 
-const path = require('path');
+// const path = require('path');
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+// const buildEslintCommand = (filenames) =>
+//   `next lint --fix --file ${filenames
+//     .map((f) => path.relative(process.cwd(), f))
+//     .join(' --file ')}`;
 
 module.exports = {
   // run next lint
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  // '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 
   // Check Typescript files
-  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
+  './src/**/*.(ts|tsx)': () => 'pnpm tsc --noEmit --pretty',
 
   // Lint and format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
+  './src/**/*.(ts|tsx|js)': (filenames) => [
     `pnpm eslint --fix ${filenames.join(' ')}`,
     `pnpm prettier --loglevel error --write ${filenames.join(' ')}`,
   ],
 
   // Format HTML, CSS, MarkDown and JSON
-  '**/*.(html|css|md|json)': (filenames) =>
+  './src/**/*.(html|css|md|json)': (filenames) =>
     `pnpm prettier --write ${filenames.join(' ')}`,
 };
