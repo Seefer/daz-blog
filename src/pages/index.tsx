822ce3bce8d42130edd0aa79bb056599082253fb
Darren Evans <seefer@outlook.com>
2023-03-01 11:32:30 +0000

    build: adding lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import Link from 'next/link';
 
 // eslint-disable-next-line no-unused-vars
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const test = true;
